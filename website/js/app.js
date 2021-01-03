ab4b5ea21fb4cff325a5e0fc2717158423719ab1
Qasim Iqbal <iqbalqasim46@gmail.com>
2021-01-03 15:57:50 -0500

    first iteration of website design is done

---
+++ website/js/app.js
@@ -0,0 +1,29 @@
+const Http = new XMLHttpRequest()
+const url= 'http://localhost:5000/get_labels'
+
+Http.open("POST", url)
+
+Http.onreadystatechange=function(){
+    if(this.readyState==4 && this.status==200){
+        console.log(Http.responseText)
+    }
+}
+
+var folder = "./assets/images/";
+
+function LoadImagesOnPage(){
+    $.ajax({
+        url : folder,
+        success: function (data) {
+            $(data).find("a").attr("href", function (i, val) {
+                if( val.match(/\.(jpe?g|png|gif|jfif)$/) ) { 
+                    $("#photos").append( "<img src='"+ folder + val +"'>" );
+                } 
+            });
+        }
+    });
+}
+
+
+
+window.onload = LoadImagesOnPage()
